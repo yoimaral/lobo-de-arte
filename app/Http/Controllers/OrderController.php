7a38b409a13c519fbdefc82f7f5b057df9a8caf4
yoimar <yalozano9@misena.edu.co>
2020-10-10 15:56:45 -0500

    refactor(- Se agrega al controlador de OrderController las funciones de edit y update)

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -110,4 +110,26 @@ class OrderController extends Controller
         return redirect()->back()->cookie($cookie);
     }
 
+        /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
 }
