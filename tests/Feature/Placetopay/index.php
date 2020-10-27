a33e32addf3ed909b64ecbe8f075bc775c9585aa
yoimar <yalozano9@misena.edu.co>
2020-10-27 10:23:57 -0500

    feat: add new test

---
+++ tests/Feature/Placetopay/index.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Placetopay;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class index extends TestCase
+{
+    //use RefreshDatabase;
+
+    /** @test */
+    public function it_can_test()
+    {
+        //$this->withoutExceptionHandling();
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+}
