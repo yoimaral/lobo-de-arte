e4b2c9bd2d7bd78b779d667f64e2831bcc68a095
yoimar <yalozano9@misena.edu.co>
2020-11-17 17:53:31 -0500

    feat: Nueva funcion de control de excepciones en la clase Handler para que los errores los devuelva en formato Json al momento de enviar informacion novalida o inexistente

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,6 +55,27 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+
+        if($exception instanceof ModelNotFoundException){
+            return response()->json(["res" => false, "error" => "Error de modelo"], 400);
+        }
+
+        if($exception instanceof QueryException){
+            return response()->json(["res" => false, "message" => "Error de consulta BDD " , $exception->getMessage()], 500);
+        }
+
+        if($exception instanceof HttpException){
+            return response()->json(["res" => false, "message" => "Error de ruta"], 404);
+        }
+
+        if($exception instanceof AuthenticationException){
+            return response()->json(["res" => false, "message" => "Error de autenticación"], 401);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->json(["res" => false, "message" => "Error de autorización, no tiene permisos"], 403);
+        }
+
         return parent::render($request, $exception);
     }
 }
