3683d5597063f7c0e80facf7341082eaddc40fd7
yoimar <yalozano9@misena.edu.co>
2020-11-18 15:51:44 -0500

    feat: Se protegen las rutas API con el middleware

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('products','ProductController');
+Route::middleware('auth:api')->get('/user', function (Request $request){
+    return $request->user();
+});
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::apiResource('products','ProductController');
+});
