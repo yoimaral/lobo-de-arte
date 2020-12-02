8dbba184da11e60269d91d7993d281ccbda879a2
yoimar <yalozano9@misena.edu.co>
2020-12-02 12:28:41 -0500

    feat: new file api Tasks IndexTasksTest

---
+++ tests/Feature/Api/Tasks/IndexTasksTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Api\Tasks;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexTasksTest extends TestCase
+{
+    //use RefreshDatabase;
+
+    /** @test */
+    public function it_can_test()
+    {
+        //$this->withoutExceptionHandling();
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+}
